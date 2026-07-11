6afa497a3134ff84e55d300a3187f15452c30ad4
mailesrsm <145134755+mailesrsm@users.noreply.github.com>
2026-07-11 18:39:18 +0530

    Time: 2 ms (51.72%), Space: 44.6 MB (7.17%) - LeetHub

---
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int numberOfSpecialChars(String word) {
+        int count=0;
+        for(char ch='a';ch<='z';ch++){
+            if(word.contains(""+ch)&&
+            word.contains(""+Character.toUpperCase(ch))){
+                count++;
+            }
+        }
+        return count;
+    }
+}
