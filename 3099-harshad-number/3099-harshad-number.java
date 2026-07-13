748ddc2884d405c19388fe721252e5834d7db98b
mailesrsm <145134755+mailesrsm@users.noreply.github.com>
2026-07-13 16:21:58 +0530

    Time: 0 ms (100%), Space: 42.2 MB (24.08%) - LeetHub

---
+++ 3099-harshad-number/3099-harshad-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int sumOfTheDigitsOfHarshadNumber(int x) {
+        int sum=0;
+        int temp=x;
+        while(x>0){
+            sum=sum+x%10;
+            x=x/10;
+        }
+        if(temp%sum==0){
+            return sum;
+        }
+        return -1;
+    }
+}
