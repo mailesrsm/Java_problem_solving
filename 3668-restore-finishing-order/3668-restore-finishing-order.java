0ce1aa89c1fd682c8f58f1d6fc74bb1f5464c5b7
mailesrsm <145134755+mailesrsm@users.noreply.github.com>
2026-08-13 20:20:14 +0530

    Time: 2 ms (52.97%), Space: 47 MB (9.74%) - LeetHub

--- 3668-restore-finishing-order/3668-restore-finishing-order.java
+++ 3668-restore-finishing-order/3668-restore-finishing-order.java
@@ -1,16 +1,17 @@
 class Solution {
     public int[] recoverOrder(int[] order, int[] friends) {
+        HashMap<Integer,Integer> map=new HashMap<>();
         int k=0;
-        int[] result=new int[friends.length];
-        for(int i=0;i<order.length;i++){
-            for(int j=0;j<friends.length;j++){
-                if(order[i]==friends[j]){
-                    result[k]=order[i];
-                    k++;
-                }
+        int[] ans=new int[friends.length];
+        for(int i=0;i<friends.length;i++){
+            map.put(friends[i],1);
+        }
+        for(int x : order){
+            if(map.containsKey(x)){
+                ans[k]=x;
+                k++;
             }
         }
-        return result;
-        
+        return ans;
     }
 }
