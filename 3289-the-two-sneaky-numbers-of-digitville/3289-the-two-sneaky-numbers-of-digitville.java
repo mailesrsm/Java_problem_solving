71c5b374bd569d1975d3ef24b2af27b846e0eb6f
mailesrsm <145134755+mailesrsm@users.noreply.github.com>
2026-08-15 19:09:55 +0530

    Time: 1 ms (99.63%), Space: 45.8 MB (60.04%) - LeetHub

---
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] getSneakyNumbers(int[] nums) {
+        int[] result=new int[2];
+        int count=0;
+        int k=0;
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    result[k]=nums[i];
+                    k++;
+                }
+            }
+        }
+        return result;
+    }
+}
